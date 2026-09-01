d7695d119241a937667656027b50d2f54585c344
ZrlofO <youngyj0928@cau.ac.kr>
2026-09-01 23:54:19 +0900

    주문해라 초기 소스 추가

---
+++ src/app/order/[token]/page.tsx
@@ -0,0 +1,7 @@
+import CustomerOrder from "@/components/CustomerOrder";
+
+export default async function OrderPage({ params }: { params: Promise<{ token: string }> }) {
+  const { token } = await params;
+  return <CustomerOrder token={token} />;
+}
+
