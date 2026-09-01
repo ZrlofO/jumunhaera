b41bef7a3199d7f298ea9bc3caa9446e454204c4
ZrlofO <youngyj0928@cau.ac.kr>
2026-09-01 23:54:23 +0900

    주문해라 초기 소스 추가

---
+++ src/app/owner/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import OwnerDashboard from "@/components/OwnerDashboard";
+import { isOwnerRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function OwnerDashboardPage() {
+  if (!(await isOwnerRequest())) redirect("/order/table-1");
+  return <OwnerDashboard />;
+}
+
